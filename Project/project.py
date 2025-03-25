f97d673677c3b907d4ef4db278f6d9128e064aba
veretennikovalexey <raidex@yandex.ru>
2025-03-25 20:54:35 +0300

    .5

--- Project/project.py
+++ Project/project.py
@@ -7,4 +7,4 @@ iter_tqdm = tqdm(range(total))
 for i in iter_tqdm:
 #    print(f"sin({i/100}) = {np.sin(i/100):.4f}")
     iter_tqdm.set_description(f"x = {i/100}, sin({i/100}) = {np.sin(i/100):.4f}")
-    time.sleep(1)
+    time.sleep(.5)
